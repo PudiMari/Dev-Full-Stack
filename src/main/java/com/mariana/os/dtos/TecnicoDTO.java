31c973e843dea127b424949d3ead91e55857e0a2
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-19 13:27:12 -0400

    Adiciona validação do DTO de técnico

--- src/main/java/com/mariana/os/dtos/TecnicoDTO.java
+++ src/main/java/com/mariana/os/dtos/TecnicoDTO.java
@@ -6,57 +6,63 @@ import org.hibernate.validator.constraints.br.CPF;
 
 import com.mariana.os.domain.Tecnico;
 
+import javax.validation.constraints.NotEmpty;
+
 public class TecnicoDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
-	private Integer id;
-	private String nome;
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    @NotEmpty(message = "O campo NOME é requerido")
+    private String nome;
 
-	@CPF
-	private String cpf;
-	private String telefone;
+    @CPF
+    @NotEmpty(message = "O campo CPF é requerido")
+    private String cpf;
 
-	public TecnicoDTO() {
-		super();
-	}
+    @NotEmpty(message = "O campo TELEFONE é requerido")
+    private String telefone;
 
-	public TecnicoDTO(Tecnico obj) {
-		super();
-		this.id = obj.getId();
-		this.nome = obj.getNome();
-		this.cpf = obj.getCpf();
-		this.telefone = obj.getTelefone();
-	}
+    public TecnicoDTO() {
+        super();
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public TecnicoDTO(Tecnico obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.telefone = obj.getTelefone();
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	public String getTelefone() {
-		return telefone;
-	}
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 
 }
