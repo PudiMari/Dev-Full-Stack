c8a71f674df5f1d7cfc9882ab77b2da0e9a16b65
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-19 13:23:57 -0400

    Adiciona endpoint para atualizar técnico

--- src/main/java/com/mariana/os/services/TecnicoService.java
+++ src/main/java/com/mariana/os/services/TecnicoService.java
@@ -12,36 +12,52 @@ import com.mariana.os.repositories.TecnicoRepository;
 import com.mariana.os.services.exceptions.DataIntegratyViolationException;
 import com.mariana.os.services.exceptions.ObjectNotFoundException;
 
+import javax.validation.Valid;
+
 @Service
 public class TecnicoService {
 
-	@Autowired
-	private TecnicoRepository repository;
+    @Autowired
+    private TecnicoRepository repository;
 
-	public Tecnico findById(Integer id) {
+    public Tecnico findById(Integer id) {
 
-		// Pode encontrar ou não
-		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id:" + id + ", Tipo: " + Tecnico.class.getName()));
-	}
+        // Pode encontrar ou não
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id:" + id + ", Tipo: " + Tecnico.class.getName()));
+    }
 
-	public List<Tecnico> findAll() {
-		return repository.findAll();
-	}
-	
-	public Tecnico create(TecnicoDTO objDTO) {
-		if (findByCPF(objDTO) != null) {
-			throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
-		}
-		return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
-	}
-	
-	private Tecnico findByCPF(TecnicoDTO objDTO) {
-		Tecnico obj = repository.findByCPF(objDTO.getCpf());
-		if (obj != null) {
-			return obj;
-		}
-		return null;
-	}
+    public List<Tecnico> findAll() {
+        return repository.findAll();
+    }
+
+    public Tecnico create(TecnicoDTO objDTO) {
+        if (findByCPF(objDTO) != null) {
+            throw new DataIntegratyViolationException("CPF já cadastrado na base de dados!");
+        }
+        return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+    }
+
+    public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+        Tecnico oldObj = findById(id);
+
+        if (findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
+            throw new DataIntegratyViolationException("CPF já cadastrado!");
+        }
+
+        oldObj.setNome(objDTO.getNome());
+        oldObj.setCpf(objDTO.getCpf());
+        oldObj.setTelefone(objDTO.getTelefone());
+        return repository.save(oldObj);
+    }
+
+    private Tecnico findByCPF(TecnicoDTO objDTO) {
+        Tecnico obj = repository.findByCPF(objDTO.getCpf());
+        if (obj != null) {
+            return obj;
+        }
+        return null;
+    }
+
 }
