4a9c500f236a5a2cea58cd92a4071835188a031c
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-19 17:10:00 -0400

    Adiciona CRUD de clientes

---
+++ src/main/java/com/mariana/os/resources/ClienteResource.java
@@ -0,0 +1,57 @@
+package com.mariana.os.resources;
+
+import com.mariana.os.domain.Cliente;
+import com.mariana.os.dtos.ClienteDTO;
+import com.mariana.os.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> findById(@PathVariable Integer id) {
+        ClienteDTO objDTO = new ClienteDTO(service.findById(id));
+        return ResponseEntity.ok().body(objDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll() {
+        List<Cliente> list = service.findAll();
+        List<ClienteDTO> listDTO = new ArrayList<>();
+
+        list.forEach(obj -> listDTO.add(new ClienteDTO(obj)));
+
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> create(@Valid @RequestBody ClienteDTO objDTO) {
+        Cliente newObj = service.create(objDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> update(@PathVariable Integer id, @Valid @RequestBody ClienteDTO objDTO) {
+        ClienteDTO newObj = new ClienteDTO(service.update(id, objDTO));
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
