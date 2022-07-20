cc328316680f6882e810e1653ae6a76ec9847c18
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-20 08:29:51 -0400

    Adiciona pesquisa por ID de ordem de serviço

---
+++ src/main/java/com/mariana/os/services/OSService.java
@@ -0,0 +1,21 @@
+package com.mariana.os.services;
+
+import com.mariana.os.domain.OS;
+import com.mariana.os.repositories.OSRepository;
+import com.mariana.os.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OSService {
+
+    @Autowired
+    private OSRepository repository;
+
+    public OS findById(Integer id) {
+        Optional<OS> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo:" + OS.class.getName()));
+    }
+}
