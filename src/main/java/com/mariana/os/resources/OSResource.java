50a2f94747e96d766edd3591c781b748cbe48279
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-20 08:59:18 -0400

    Adiciona listagem completa de ordens de serviço

--- src/main/java/com/mariana/os/resources/OSResource.java
+++ src/main/java/com/mariana/os/resources/OSResource.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/os")
 public class OSResource {
@@ -20,5 +23,12 @@ public class OSResource {
     public ResponseEntity<OSDTO> findById(@PathVariable Integer id){
         OSDTO obj = new OSDTO(service.findById(id));
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OSDTO>> findAll(){
+        List<OSDTO> list = service.findAll().stream()
+                .map(OSDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/mariana/os/services/OSService.java
+++ src/main/java/com/mariana/os/services/OSService.java
@@ -6,6 +6,7 @@ import com.mariana.os.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class OSService {
     public OS findById(Integer id) {
         Optional<OS> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo:" + OS.class.getName()));
+    }
+
+    public List<OS> findAll(){
+        return repository.findAll();
     }
 }
