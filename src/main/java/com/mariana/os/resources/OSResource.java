3fa5a8f717a467cedeeec78c4f6e1902cbc8c2aa
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-20 08:28:43 -0400

    Adiciona repositório para ordens de serviço

---
+++ src/main/java/com/mariana/os/resources/OSResource.java
@@ -0,0 +1,24 @@
+package com.mariana.os.resources;
+
+import com.mariana.os.dtos.OSDTO;
+import com.mariana.os.services.OSService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/os")
+public class OSResource {
+
+    @Autowired
+    private OSService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OSDTO> findById(@PathVariable Integer id){
+        OSDTO obj = new OSDTO(service.findById(id));
+        return ResponseEntity.ok().body(obj);
+    }
+}
